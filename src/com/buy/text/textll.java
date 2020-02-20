58cfa163d5f7dc791088adca109981c9cb0bb0b7
889 <2517859189@qq.com>
2020-02-20 11:42:53 +0800

    1.2020-2-20易买网项目第二次提交 共计第四次 2.添加了hemo.jsp

---
+++ src/com/buy/text/textll.java
@@ -0,0 +1,22 @@
+package com.buy.text;
+
+import com.buy.dao.product.IProductCategory;
+import com.buy.dao.product.ProductCategoryImpl;
+import com.buy.entity.EasybuyProductCategory;
+import com.buy.service.product.IProductCategoryService;
+import com.buy.service.product.ProductCategoryServiceImpl;
+
+import java.util.List;
+
+public class textll {
+    public static void main(String[] args) {
+        IProductCategoryService productCategory=new ProductCategoryServiceImpl();
+        List<EasybuyProductCategory> productCategories= productCategory.queryAllProductCategory("0");
+        for (EasybuyProductCategory category: productCategories) {
+            System.out.println(category.getName());
+
+        }
+
+
+    }
+}
